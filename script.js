3b329d326dbaa371de9668329465d4265d2b7500
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-09-15 19:42:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var themebutton = document.getElementById('corT');
 
 function themeButton(){
 ///if(theme.href == './light.css'){
-  theme.href = './dark.css';
+///  theme.href = './dark.css';
   themebutton.innerHTML = 'Light';
 ///  }
 }
