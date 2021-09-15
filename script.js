c249c040a97cd673063a754390f53b84b48cbcf5
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-09-15 19:27:50 -0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var theme = document.getElementsByTagName('link');
 var themebutton = document.getElementById('corT');
 
-Function ThemeButton(){
+function ThemeButton(){
 ///if(theme.href == './light.css'){
   theme.href = './dark.css';
   themebutton.innerHTML = 'Light';
