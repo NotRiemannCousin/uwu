51ab1f8451e9e7a6799739abae097a3e645c224b
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-09-16 09:00:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function themeButton(){
 if(document.getElementById('corT').innerHTML == 'Dark'){
-    document.getElementByTagName('link').href = './dark.css';
+    document.getElementsByTagName('link').href = './dark.css';
     document.getElementById('corT').innerHTML = 'Light';
   }else{
-    document.getElementByTagName('link').href = './light.css';
+    document.getElementsByTagName('link').href = './light.css';
     document.getElementById('corT').innerHTML = 'Dark';
   }
 }
