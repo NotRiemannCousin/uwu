b2abd519554a21cd2daecf274ad002cd225d4ca6
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-09-30 01:01:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const list;
+/*const list;
 fetch("list.json")
       .then((response) => {
            return response.json();
       }).then(js => {
            list = js;
            document.getElementById('todosanimes').innerHTML = js[0].name;
-      })
+      })*/
 function dark (){ //Botão cor tema
     document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='dark.css'/><button onclick='light()'>Light</button>")
 }
