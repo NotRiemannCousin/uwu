2ae0b308d81242310fa866ba8b2ab66382d917c9
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-10-02 21:25:15 -0300

    script.js

--- script.js
+++ script.js
@@ -1,98 +1,52 @@
-function nameInitialArray(anime, char, name){
-
-  console.log(name);
-
-  console.log("");
-
+function nameInitialArray(anime, char){
   anime.map(function(element){
 
     if(element.name[0] == char[0]){
-
-      console.log(element.name);
-
     }
-
-  }
-
-           );
-
+);
 }
 
 function readArray(array, compar, atribute){
-
   var string = [];
-
   for(let t in Object.keys(array[0])){
-
     if(Object.keys(array[0])[t] == atribute){
-
       array.map(function(element){
-
         if(Object.values(element)[t] == compar){
-
           string.push(element.name + ' \n');
-
         }
-
-      }
-
-               );
-
-      break;
-
-    }
-
+      }         
+   );
+   break;
   }
-
-  return string;
-
+ }
+ return string;
 }
 
 
 var list;
 
 fetch('list.json')
-
   .then(response => {
-
   return response.json().then(js => {
-
     list = js;
-
     document.getElementById('todosanimes').innerHTML = list.map(x => {return x.name + ' \n';}                                                             );
-
     document.getElementById('osanimes').innerHTML = readArray(list, 'anime', 'type');
-
     document.getElementById('movies').innerHTML = readArray(list, 'movie', 'type');
 
-    
-
-    
-
   document.getElementById('botoes').hidden =true;
-
   document.getElementById('todosanimes').hidden = true;           
-
   document.getElementById('osanimes').hidden = true;
-
   document.getElementById('movies').hidden = true;
-
   });});
 
 function showHide(obj){
-
   document.getElementById('botoes').hidden =true;
-
   document.getElementById('todosanimes').hidden = true;
-
   document.getElementById('osanimes').hidden = true;
-
   document.getElementById('movies').hidden = true;
 
   document.getElementById(obj).hidden = false;
-
 }
-
 
 function Dark (){ //Botão cor tema
     document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='dark.css'/><button onclick='Light()'>Light</button>")
