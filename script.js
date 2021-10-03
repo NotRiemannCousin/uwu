d00bc98f56e8f954db6b5e122dc4c2f82644ba39
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-10-02 22:00:56 -0300

    script.js

--- script.js
+++ script.js
@@ -3,29 +3,27 @@ function nameInitialArray(anime, char){
 
     if(element.name[0] == char[0]){
     }
-);
+  });
 }
 
 function readArray(array, compar, atribute){
   var string = [];
   for(let t in Object.keys(array[0])){
     if(Object.keys(array[0])[t] == atribute){
-      array.map(function(element){
+      array.map(element=>{
         if(Object.values(element)[t] == compar){
           string.push(element.name + ' \n');
         }
-      }         
-   );
+      });
    break;
-  }
+    }
  }
  return string;
 }
 
-
 var list;
 
-fetch('list.json')
+fetch('https://raw.githubusercontent.com/MarceloH1122/uwu/main/list.json')
   .then(response => {
   return response.json().then(js => {
     list = js;
@@ -49,8 +47,8 @@ function showHide(obj){
 }
 
 function Dark (){ //Botão cor tema
-    document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='dark.css'/><button onclick='Light()'>Light</button>")
+    document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='dark.css'/><button onclick='Light()'>Light</button>");
 }
 function Light (){
-    document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='light.css'/><button onclick='Dark()'>Dark</button>")
+    document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='light.css'/><button onclick='Dark()'>Dark</button>");
 }
