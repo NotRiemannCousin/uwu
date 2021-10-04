e9a7708d17b304dc484476922b95a546558f9892
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-10-04 15:46:58 -0300

    script.js

--- script.js
+++ script.js
@@ -14,22 +14,24 @@ function readArray(array, compar, atribute){
         if(Object.values(element)[t] == compar){
           string.push(element.name);
         }
-      });
-   break;
+      }
+               );
+      break;
     }
- }
- return string.join('<hr>');
+  }
+  return string.join("<hr>");
 }
-
 var list;
-
-fetch('list.json')
+fetch('https://raw.githubusercontent.com/MarceloH1122/uwu/main/list.json')
   .then(response => {
   return response.json().then(js => {
     list = js;
-    document.getElementById('todosanimes').innerHTML = list.map(x => {return x.name + ' \n';} ).join('<hr>');                                                            );
+    document.getElementById('todosanimes').innerHTML = list.map(x => {
+      return x.name + ' \n';}).join("<hr>");
+    
     document.getElementById('osanimes').innerHTML = readArray(list, 'anime', 'type');
     document.getElementById('movies').innerHTML = readArray(list, 'movie', 'type');
+
 
   document.getElementById('botoes').hidden = true;
   document.getElementById('todosanimes').hidden = true;           
