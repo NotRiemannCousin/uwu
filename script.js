ddabc867e1bb176d5c8287f61c8265b195461dae
Marcelo Henrique <68703999+MarceloH1122@users.noreply.github.com>
2021-12-18 05:54:39 -0300

    script.js

--- script.js
+++ script.js
@@ -49,14 +49,14 @@ fetch('https://raw.githubusercontent.com/MarceloH1122/uwu/main/list.json')
     document.getElementById('movies').innerHTML = readArray(list, 'movie', 'type');
 
 
-  document.getElementById('botoes').hidden = true;
+  document.getElementById('letras').hidden = true;
   document.getElementById('todosanimes').hidden = true;           
   document.getElementById('osanimes').hidden = true;
   document.getElementById('movies').hidden = true;
   });});
 
 function showHide(obj){
-  document.getElementById('botoes').hidden = true;
+  document.getElementById('letras').hidden = true;
   document.getElementById('todosanimes').hidden = true;
   document.getElementById('osanimes').hidden = true;
   document.getElementById('movies').hidden = true;
